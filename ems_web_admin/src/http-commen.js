3586048a2cc40a05b9689527286667d3da8e1857
usha <ushathapabhopal@gmail.com>
2021-11-29 19:08:20 +0530

    userList

--- ems_web_admin/src/http-commen.js
+++ ems_web_admin/src/http-commen.js
@@ -1,9 +1,10 @@
-import axios from "axios";
-import './Services/EMSService';
+module.exports= {
+  BASE_URL: "http://localhost:3001"
+}
 
-export default axios.create({
-  baseURL: "http://localhost:3001/api",
-  headers: {
-    "Content-type": "application/json"
-  }
-});
+// export default axios.create({
+//   baseURL: "http://localhost:3001",
+//   headers: {
+//     "Content-type": "application/json"
+//   }
+// });
